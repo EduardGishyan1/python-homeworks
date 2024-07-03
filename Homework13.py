2acd494e956abb2df618209d6b19c3fe85d2aa67
EduardGishyan1 <164676522+EduardGishyan1@users.noreply.github.com>
2024-07-03 18:58:22 +0400

    Update Homework13.py

--- Homework13.py
+++ Homework13.py
@@ -1,1 +1,31 @@
+# Task1
 
+# matrix = [[1, 2, 3],
+#           [4, 5, 6],
+#           [7, 8, 9]]
+
+# transposed = [[0, 0, 0],
+#               [0, 0, 0],
+#               [0, 0, 0]]
+
+# for i in range(len(matrix)):
+#     for j in range(len(matrix[0])):
+#         transposed[j][i] = matrix[i][j]
+
+# print(transposed)
+
+# Task2
+
+# matrix = [
+#     [1, 2, 3, 4],
+#     [5, 6, 7, 8],
+#     [9, 10, 11, 12],
+#     [13, 14, 15, 16]
+# ]
+
+# for i in range(len(matrix) - 1, -1, -1):
+#     for j in range(len(matrix[i]) - 1, -1, -1):
+#         if j == 0:
+#             print(matrix[i][j])
+#         else:
+#             print(matrix[i][j], end=' ')
