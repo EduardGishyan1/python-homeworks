6ad7a8f306654e8ba04a9bd4e873985750da7cd6
EduardGishyan1 <164676522+EduardGishyan1@users.noreply.github.com>
2024-07-08 15:18:46 +0400

    Add files via upload

---
+++ Homework15.py
@@ -0,0 +1,66 @@
+reserved = ["admin","root"]
+users = {
+    'username':None, 'email': None, 'password': None, 'phone_number': None
+}
+list = []
+flag = True
+users_count = input("enter quantity of users: ")
+if users_count.isdigit():
+        for i in range(int(users_count)):
+         for i in range(int(users_count)):
+            for key in users:
+            
+                if key == "username":
+                     username = input("enter username: ")
+                     if len(username) > 5 and len(username) < 20 and username.isalnum() and username.lower() not in reserved:
+                          users["username"] = username
+                     else:
+                          print("Should be between 5 and 20 characters long.Can only contain alphanumeric characters.Should not be a reserved name")
+                          flag = False
+                          break
+                elif key == "email":
+                     email = input("enter email: ")
+                     if "@" in email and not email.startswith("@") and not email.endswith("@"):
+                          users["email"] = email
+                     else:
+                        print("Should have a valid email format (e.g., user@example.com).")
+                        flag = False
+                        break
+                elif key == "password":
+                     password = input("enter password: ")
+                     sign = [i for i in password if i in "!@#$%^&*"]
+                     isuppercase = [j for j in password if ord(j) > 65 and ord(j) < 97]
+                     islowercase = [k for k in password if ord(k) > 97 and ord(k) < 122]
+                     repeat_password = input("repeat password: ")
+                     if len(password) >= 8:
+                        if sign and isuppercase and islowercase:
+                            if password == repeat_password:
+                                users["password"] = password
+                            else:
+                                print("repeat password is incorrect")
+                                flag = False
+                                break
+                        else:
+                            print("incorrect password")
+                            flag = False
+                            break
+                     else:
+                        print("Should be at least 8 characters long.Must contain at least one uppercase letter, one lowercase letter, one digit, and one special character (e.g., !@#$%^&*).")
+                        flag = False
+                        break
+                elif key == "phone_number":
+                    phone_number = input("enter phone_number: ")
+                    if phone_number.isdigit() and phone_number.startswith("0") or phone_number.startswith("+"):
+                        users["phone_number"] = phone_number
+                    else:
+                        print("The number should only contain digits, with optional ‘+’ at the beginning")
+                        flag = False
+                        break 
+        list.append(users.copy())
+else:
+    print("quantity must be valid number")
+    exit()
+if flag:
+    print(list)
+else:
+    print("invalid registration")
