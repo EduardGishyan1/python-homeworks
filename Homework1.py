0e8c303fb385290588e72b35d8bc36664ede781a
EduardGishyan <164676522+EduardGishyan1@users.noreply.github.com>
2024-09-18 17:25:54 +0400

    Update Homework1.py

--- Homework1.py
+++ Homework1.py
@@ -4,6 +4,6 @@ print("Eduard Gishyan")
 number = float(input("Enter number"))
 
 if number % 2 == 0:
-     print("number is odd")
-else:
      print("number is even")
+else:
+     print("number is odd")
